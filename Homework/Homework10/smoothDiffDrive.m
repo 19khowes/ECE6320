6375d5c73d31d0c2dd05786657a68e5192d7373a
Kade Howes <19khowes@gmail.com>
2024-11-07 12:58:43 -0700

    HW10 first draft of extra

--- Homework/Homework10/smoothDiffDrive.m
+++ Homework/Homework10/smoothDiffDrive.m
@@ -3,15 +3,20 @@ clear; close all;
 x0 = [0; 0; 0; 0; 0];
 
 % Define parameters
-P.r = 10;
-P.L = 10;
+P.r = 3;
+P.L = 5;
+P.A = [0 0 1 0; 0 0 0 1; 0 0 0 0; 0 0 0 0];
+P.B = [0 0; 0 0; 1 0; 0 1];
+P.K = place(P.A,P.B,[-1 -2 -3 -4]);
+P.ep = 100;
 % Simulate the state forward in time the state
 
 dt = 0.01;
 t = [0:dt:10];
 % xd = xeq;
 % u = @(t,x,P) ueq-K*(x-xeq);
-u = @(t,x,P) [1;0];
+% u = @(t,x,P) [1;2];
+u = @(t,x,P) u_function(t,x,P);
 
 [tmat, xmat] = ode45(@(t,x)f(t,x,u,P), t, x0);
 tmat = tmat';
@@ -26,28 +31,57 @@ fontsize = 12;
 figure;
 subplot(5,1,1);
 plot(tmat, xmat(1,:), 'b', 'linewidth', 3);
-ylabel('$\x 1(t)$', 'fontsize', fontsize, 'Interpreter','latex');
+ylabel('$x_1(t)$', 'fontsize', fontsize, 'Interpreter','latex');
 set(gca, 'fontsize', fontsize);
 
 subplot(5,1,2);
 plot(tmat, xmat(2,:), 'b', 'linewidth', 3);
-ylabel('$\x 2(t)$', 'fontsize', fontsize, 'Interpreter','latex');
+ylabel('$x_2(t)$', 'fontsize', fontsize, 'Interpreter','latex');
 set(gca, 'fontsize', fontsize);
 
 subplot(5,1,3);
 plot(tmat, xmat(3,:), 'b', 'linewidth', 3);
-ylabel('$\x 2(t)$', 'fontsize', fontsize, 'Interpreter','latex');
+ylabel('$\theta(t)$', 'fontsize', fontsize, 'Interpreter','latex');
 set(gca, 'fontsize', fontsize);
 
 subplot(5,1,4);
 plot(tmat, xmat(4,:), 'b', 'linewidth', 3);
-ylabel('$\x 2(t)$', 'fontsize', fontsize, 'Interpreter','latex');
+ylabel('$w_r(t)$', 'fontsize', fontsize, 'Interpreter','latex');
 set(gca, 'fontsize', fontsize);
 
 subplot(5,1,5);
 plot(tmat, xmat(5,:), 'b', 'linewidth', 3);
-ylabel('$\x 2(t)$', 'fontsize', fontsize, 'Interpreter','latex');
+ylabel('$w_l(t)$', 'fontsize', fontsize, 'Interpreter','latex');
 set(gca, 'fontsize', fontsize);
+
+%% input
+function abar = u_function(t,x,P)
+    r = P.r;
+    L = P.L;
+    % get states
+    q1 = x(1);
+    q2 = x(2);
+    theta = x(3);
+    wr = x(4);
+    wl = x(5);
+
+    v = (r/2)*(wr+wl);
+    w = (r/L)*(wr-wl);
+    
+    z = [q1+P.ep*cos(theta);
+         q2+P.ep*sin(theta);
+         v*cos(theta)+P.ep*w*sin(theta);
+         v*sin(theta)+P.ep*w*cos(theta);];
+
+    zd = [sin(t); t; cos(t); 1];
+    
+    u = [-sin(t); 0] - P.K*(z-zd);
+
+    Re = [cos(theta) -P.ep*sin(theta); sin(theta) P.ep*cos(theta)];
+    what = [0 -P.ep*w; w/P.ep 0];
+    vbar = [v; w];
+    abar = Re^-1*u - what*vbar;
+end
 
 %% dynamics
 function xdot = f(t,x,u_function,P)
