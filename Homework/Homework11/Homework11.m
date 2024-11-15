179c2cce889cea70cf35ba95f522a7aeb544887c
Kade Howes <19khowes@gmail.com>
2024-11-14 17:07:53 -0700

    do decomp to check. HW11 done

--- Homework/Homework11/Homework11.m
+++ Homework/Homework11/Homework11.m
@@ -29,13 +29,29 @@ uff = [u1;u2];
 eqn = B*uff + A*xd == 0;
 uff_sol = solve(eqn,uff);
 uff_sol = double([uff_sol.u1, uff_sol.u2]);
-rGamma = rank(ctrb(A,B)); % check controllability
+Gamma = ctrb(A,B);
+rGamma = rank(Gamma); % check controllability
 maxX = [5; 3; 1];
 maxU = [10; 1];
 Q = diag(1./(maxX.^2)); % Bryson's method
 R = diag(1./(maxU.^2));
-K = lqr(A,B,Q,R)
-L = place(A',C',[-100, -200, -300])'
+K = lqr(A,B,Q,R);
+L = place(A',C',[-100, -200, -300])';
+% check answer doing controllable decomposition
+v = orth(Gamma);
+w = null(Gamma');
+T = [v w];
+Ahat = T^-1*A*T;
+Bhat = T^-1*B;
+A11hat = Ahat(1:rGamma,1:rGamma);
+B1hat = Bhat(1:rGamma,:);
+% transform Q
+Qhat = T'*Q*T;
+Q11hat = Qhat(1:rGamma,1:rGamma);
+K1hat = lqr(A11hat,B1hat,Q11hat,R);
+Khat = [K1hat [0;0]];
+Kcheck = Khat*T^-1; % matches original K using just lqr!
+
 
 
 
